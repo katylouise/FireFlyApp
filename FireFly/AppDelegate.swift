48b1a20d31cee33cc479924c67dcc6e845c16f3a
Rebecca Appleyard <rklappleyard@gmail.com>
2015-08-26 14:13:56 +0100

    Initial commit

--- FireFly/AppDelegate.swift
+++ FireFly/AppDelegate.swift
@@ -5,7 +5,8 @@
 //  Created by Rebecca Appleyard on 26/08/2015.
 //  Copyright (c) 2015 Firefly. All rights reserved.
 //
-
+import Parse
+import Bolts
 import UIKit
 
 @UIApplicationMain
@@ -15,7 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        Parse.enableLocalDatastore()
+        Parse.setApplicationId("yQ9ARKDFZjy9S3Qz3Va9uT76AbQm7rok6tL8YcwJ", clientKey: "8VmUpqJfqdXlxRBRPD5DDBcGLuXQsazysWEx9kEw")
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
+        let testObject = PFObject(className: "TestObject")
+        testObject["foo"] = "bar"
+        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            println("Object has been saved.")
+        }
         return true
     }
 
---
+++ FireFly/TableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewCell.swift
+//  FireFly
+//
+//  Created by Rebecca Appleyard on 26/08/2015.
+//  Copyright (c) 2015 Firefly. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+  
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
